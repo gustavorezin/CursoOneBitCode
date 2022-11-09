b2ad97a4f2edd3fcc520ced9d8f72dbafa6dc158
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2022-11-09 11:56:35 -0300

    09-11-2022

--- JavaScript/JS I/funcoes/index.js
+++ JavaScript/JS I/funcoes/index.js
@@ -1,5 +1,55 @@
 function dobro(num) {
-    alert(`O dobro de ${num} é ${num * 2}`)
+  alert(`O dobro de ${num} é ${num * 2}`)
 }
 
 dobro(5)
+
+// Ao usar vários parâmetros, é recomendado deixar aqueles com valor padrão por último
+function criarUsuario(nome, email, senha, tipo = "leitor") {
+  const usuario = { 
+    nome, // => nome: nome
+    email, 
+    senha, 
+    tipo 
+  }
+  console.log(usuario)
+}
+  
+function novoUsuario(nome, tipo = "leitor", email, senha) {
+  const usuario = { nome, email, senha, tipo }
+  console.log(usuario)
+}
+
+criarUsuario("Isaac", "isaac@email.com", "1234")
+novoUsuario("Isaac", "isaac@email.com", "1234")
+
+// Ao criarmos uma função com muitos parâmetros,
+// uma boa prática é substituí-los por um objeto
+function parametrosDoJeitoErrado(nome, telefone, endereco, aniversario, email, senha) {
+  // ...
+}
+function parametrosDoJeitoCerto(usuario) {
+  // ...
+}
+
+// Além de facilitar na chamada da função, a ordem dos parâmetros se torna irrelevante
+parametrosDoJeitoErrado("nome", "telefone", "endereco", "aniversario", "email", "senha")
+  const dadosDoUsuario = {
+  nome: "",
+  telefone: "",
+  email: "",
+  senha: "",
+  aniversario: "",
+  endereco: ""
+}
+parametrosDoJeitoCerto(dadosDoUsuario)
+
+function maioridade(idade) {
+  if (idade >= 18) {
+    return "Maior de idade"
+  } else {
+    return "Menor de idade"
+  }
+}
+console.log(maioridade(20))
+console.log(maioridade(13))
---
+++ JavaScript/JS I/metodos/index.js
@@ -0,0 +1,8 @@
+let pessoa = {
+  nome: "Gustavo",
+  idade: 19,
+  dizerOla() {console.log(`Olá, ${this.nome}!`)}
+}
+
+console.log(pessoa)
+pessoa.dizerOla()
