fa3471006dda8557842d7c978b585ec77d27f061
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2023-02-08 21:37:38 -0300

    08-02-2023-1

--- JavaScript/JS II/DOM/praticando/calcJS/index.js
+++ JavaScript/JS II/DOM/praticando/calcJS/index.js
@@ -1,1 +1,33 @@
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+const screenCalc = document.getElementById('screen')
+const charKeys = document.querySelectorAll('.charKey')
+
+charKeys.forEach((charKey) => {
+	charKey.addEventListener('click', () => {
+		const keyValue = charKey.dataset.value
+		screenCalc.value += keyValue
+		screenCalc.focus()
+	})
+})
+
+screenCalc.addEventListener('keydown', (ev) => {
+	ev.preventDefault()
+	if (allowedKeys.includes(ev.key)) {
+		screenCalc.value += ev.key
+		return
+	}
+	if (ev.key === 'Backspace') {
+		screenCalc.value = screenCalc.value.slice(0, -1)
+	}
+	if (ev.key === 'C' || ev.key === 'c') {
+		clearScreen()
+	}
+	if (ev.key === 'Enter') {
+		screenCalc.value = eval(screenCalc.value)
+		clearScreen()
+	}
+})
+
+function clearScreen() {
+	screenCalc.value = ''
+}
