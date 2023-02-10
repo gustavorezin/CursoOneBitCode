b5b823d36cbbcda646400835c346a83e679e1ac6
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2023-02-10 14:25:56 -0300

    10-02-2023-1

--- JavaScript/JS II/DOM/praticando/calcJS/index.js
+++ JavaScript/JS II/DOM/praticando/calcJS/index.js
@@ -1,10 +1,11 @@
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 const screenCalc = document.getElementById('screen')
+const screenRecult = document.getElementById('result')
 const charKeys = document.querySelectorAll('.charKey')
 
-charKeys.forEach((charKey) => {
-	charKey.addEventListener('click', () => {
-		const keyValue = charKey.dataset.value
+charKeys.forEach((charKeyBtn) => {
+	charKeyBtn.addEventListener('click', () => {
+		const keyValue = charKeyBtn.dataset.value
 		screenCalc.value += keyValue
 		screenCalc.focus()
 	})
@@ -12,22 +13,31 @@ charKeys.forEach((charKey) => {
 
 screenCalc.addEventListener('keydown', (ev) => {
 	ev.preventDefault()
-	if (allowedKeys.includes(ev.key)) {
+	if(allowedKeys.includes(ev.key)) {
 		screenCalc.value += ev.key
-		return
 	}
-	if (ev.key === 'Backspace') {
+	if(ev.key === 'Backspace') {
 		screenCalc.value = screenCalc.value.slice(0, -1)
 	}
-	if (ev.key === 'C' || ev.key === 'c') {
-		clearScreen()
+	if(ev.ctrlKey && (ev.key === 'C' || ev.key === 'c')) {
+		clearScreenCalc()
 	}
-	if (ev.key === 'Enter') {
-		screenCalc.value = eval(screenCalc.value)
-		clearScreen()
+	if(ev.key === 'Enter') {
+		calculate()
 	}
 })
 
-function clearScreen() {
+document.getElementById('clear').addEventListener('click', clearScreenCalc)
+document.getElementById('equal').addEventListener('click', calculate)
+
+function clearScreenCalc(){
 	screenCalc.value = ''
+	screenCalc.focus()
+}
+
+function calculate() {
+	const result = eval(screenCalc.value)
+	screenCalc.value = result
+	screenRecult.value = result
+	screenCalc.focus()
 }
