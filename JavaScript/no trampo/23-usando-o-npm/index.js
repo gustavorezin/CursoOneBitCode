0df2f04ae969ddac14f0e54349585d35cc01095c
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2023-06-29 12:00:11 -0300

    29-06-2023

---
+++ JavaScript/no trampo/23-usando-o-npm/index.js
@@ -0,0 +1,12 @@
+// 1- npm init
+// 2- npm i --save lodash | --save garante que vai salva no package.json do projeto
+// 3- Utilizando o lodash
+
+const lodash = require("lodash");
+
+console.log(lodash.isArray([]));
+console.log(lodash.kebabCase("Gustavo Rezin Durigon"));
+
+// 4- node index.js | para iniciar programa
+// 5- npm i --save-dev eslint | nosso projeto só utiliza enquanto estamos codificando ele
+// 6- npm uninstall eslint | para excluir uma dependencia
